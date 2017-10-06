6919edde0f6920ed271f7cd1be7b28b1bc5a1d17
saiprashanth202 <saiprashanth.p@c2n.in>
2017-10-06 12:34:34 +0530

    JAVA Code

---
+++ ID CARDs Angular4/JAVA/src/main/java/com/c2n/dao/IProfileDAO.java
@@ -0,0 +1,13 @@
+package com.c2n.dao;
+import java.util.List;
+
+import com.c2n.entity.Profile;
+public interface IProfileDAO {
+    List<Profile> getAllProfiles();
+    Profile getProfileById(int profileId);
+    void createProfile(Profile profile);
+    void updateProfile(Profile profile);
+    void deleteProfile(int profileId);
+    boolean profileExists(String username, String email,String password,String cpassword,String mobilenumber,String address);
+}
+ 
---
+++ ID CARDs Angular4/JAVA/src/main/java/com/c2n/entity/Profile.java
@@ -0,0 +1,80 @@
+package com.c2n.entity;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="profile")
+public class Profile implements Serializable { 
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="profile_id")
+    private int profileId;  
+	@Column(name="username")
+    private String username;
+	@Column(name="password")	
+	private String password;                
+	@Column(name="cpassword")	            
+	private String cpassword;               
+	@Column(name="email")	                
+	private String email;                   
+	                                        
+	@Column(name="mobilenumber")	
+	private String mobilenumber;
+	@Column(name="address")	
+	private String address;
+	
+	
+	
+	public int getProfileId() {
+		return profileId;
+	}
+	public void setProfileId(int profileId) {
+		this.profileId = profileId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobilenumber() {
+		return mobilenumber;
+	}
+	public void setMobilenumber(String mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+} 
